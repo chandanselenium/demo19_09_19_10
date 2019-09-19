66a0e21161290b0963600f140e03dcc8888a82fb
Chandan V <chandanselenium@gmail.com>
2019-09-19 20:35:17 +0530

    second commit

--- test_demo/test_demo3.py
+++ test_demo/test_demo3.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
+import logging
 def testMethod5(setUpMethod,setUpClass):
     print('modify another user')
 
 def testMethod6(setUpMethod,setUpClass):
     driver=webdriver.Chrome()
-    print('delete another user')
+    logging.basicConfig(filename="test.log", level=logging.DEBUG)
+    logging.info('delete another user')
